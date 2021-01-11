5c2fac7a0af52080395783deefef853c603b53ad
Maxence <lemairemaxence@hotmail.com>
2021-01-11 15:39:08 +0100

    exo done

--- public/js/main.js
+++ public/js/main.js
@@ -1,18 +1,34 @@
 // EXO 1
 
-let chiffre = parseInt(prompt("Chiffre ?"))
-let compt = 0
+// let chiffre = parseInt(prompt("Chiffre ?"))
+// let compt = 0
 
-while (compt <= chiffre) {
-    console.log(compt);
-    compt++
-}
+// while (compt <= chiffre) {
+//     console.log(compt);
+//     compt++
+// }
 
 // EXO 2
 
-let tour = 3
+// 1
 
-while (tour <= 6) {
+// let tour = 0
+
+// while (tour <= 10) {
+//     console.log(tour);
+//     tour++
+// }
+
+// let tour = 3
+
+// while (tour <= 6) {
+//     console.log(tour);
+//     tour ++
+// }
+
+let tour = 0
+
+while (tour <= 20) {
     console.log(tour);
     tour += 2
 }
